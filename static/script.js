a65dfa1e409f7015c62ad98a642e3da4753da91c
Nithiksha S <nithiksha12s@gmail.com>
2026-06-28 09:20:13 +0530

    first commit

---
+++ static/script.js
@@ -0,0 +1,24 @@
+let slider=document.getElementById("n");
+
+slider.oninput=function(){
+
+let n=this.value;
+
+document.getElementById("value").innerHTML=n;
+
+document.getElementById("o1").innerHTML=1;
+
+document.getElementById("olog").innerHTML=
+Math.log2(n).toFixed(2);
+
+document.getElementById("on").innerHTML=n;
+
+document.getElementById("onlog").innerHTML=
+(n*Math.log2(n)).toFixed(2);
+
+document.getElementById("on2").innerHTML=
+n*n;
+
+}
+
+slider.oninput();
